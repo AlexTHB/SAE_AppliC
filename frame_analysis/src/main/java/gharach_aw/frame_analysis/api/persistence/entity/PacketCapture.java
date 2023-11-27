a6f6c1675b2932f55293d8752f0dadb99fbc5dfd
gj200498 <jgharach@proton.me>
2023-11-27 21:41:38 +0100

    maj

--- frame_analysis/src/main/java/gharach_aw/frame_analysis/api/persistence/entity/PacketCapture.java
+++ frame_analysis/src/main/java/gharach_aw/frame_analysis/api/persistence/entity/PacketCapture.java
@@ -10,20 +10,21 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 /**
  * The {@code PacketCapture} class represents a collection of network packets captured from Wireshark
  * and is mapped to the "packet_capture" table in the database.
  */
 @Entity
-@Table(name = "packet_capture")
+@Table(name = "packet_capture", uniqueConstraints = @UniqueConstraint(columnNames = "fileName"))
 public class PacketCapture {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "fileName")
+    @Column(name = "fileName", unique = true)
     private String fileName;
 
     @Column(name = "fileDate")
