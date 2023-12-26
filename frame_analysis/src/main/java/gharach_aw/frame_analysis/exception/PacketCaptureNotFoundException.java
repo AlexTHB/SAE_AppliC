abc3edc16f99e74b17ce0c284547670cc87fea95
jgharach <69178823+jgharach@users.noreply.github.com>
2023-12-26 17:47:08 +0100

    Delete frame_analysis directory

--- frame_analysis/src/main/java/gharach_aw/frame_analysis/exception/PacketCaptureNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package gharach_aw.frame_analysis.exception;
-
-public class PacketCaptureNotFoundException extends RuntimeException{
-    
-    public PacketCaptureNotFoundException (String message) {
-        super(message);
-    }   
-}
