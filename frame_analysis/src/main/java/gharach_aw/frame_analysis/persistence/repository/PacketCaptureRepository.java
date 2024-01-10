3014205c270f5221fcd80573d258208b4fb00e72
jgharach <69178823+jgharach@users.noreply.github.com>
2024-01-10 23:26:37 +0100

    Delete frame_analysis directory

--- frame_analysis/src/main/java/gharach_aw/frame_analysis/persistence/repository/PacketCaptureRepository.java
+++
@@ -1,9 +0,0 @@
-package gharach_aw.frame_analysis.persistence.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import gharach_aw.frame_analysis.persistence.entity.PacketCapture;
-
-public interface PacketCaptureRepository extends JpaRepository<PacketCapture, Long>{
-
-}
